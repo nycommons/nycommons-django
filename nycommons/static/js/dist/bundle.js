28e8ce5f3ad2ea9f41558bd7c1ffdcd1d42c70af
Eric Brelsford <ebrelsford@gmail.com>
2016-05-24 14:21:21 -0400

    Rough version of footer

--- nycommons/static/js/dist/bundle.js
+++ nycommons/static/js/dist/bundle.js
@@ -13300,7 +13300,7 @@ require('./Leaflet.label-patch.js');
 
 },{}],"/home/eric/Documents/596/nycommons/nycommons/static/node_modules/leaflet/dist/leaflet-src.js":[function(require,module,exports){
 /*
- Leaflet 1.0.0-rc.1 (6ea195a), a JS library for interactive maps. http://leafletjs.com
+ Leaflet 1.0.0-rc.1 (a626826), a JS library for interactive maps. http://leafletjs.com
  (c) 2010-2015 Vladimir Agafonkin, (c) 2010-2011 CloudMade
 */
 (function (window, document, undefined) {
@@ -33647,7 +33647,7 @@ function getMinNorthing(zoneLetter) {
 }
 
 },{}],"/home/eric/Documents/596/nycommons/nycommons/static/node_modules/proj4/package.json":[function(require,module,exports){
-module.exports=module.exports=module.exports=module.exports=module.exports=module.exports=module.exports=module.exports=module.exports={
+module.exports=module.exports={
   "name": "proj4",
   "version": "2.3.3",
   "description": "Proj4js is a JavaScript library to transform point coordinates from one coordinate system to another, including datum transformations.",
