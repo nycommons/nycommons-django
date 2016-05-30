73cbc34f52f23d9ee802ce7284c0dd3d6eb8b930
Eric Brelsford <ebrelsford@gmail.com>
2016-05-30 16:43:10 -0700

    CONN_MAX_AGE

--- nycommons/nycommons/settings/base.py
+++ nycommons/nycommons/settings/base.py
@@ -39,6 +39,7 @@ DATABASES = {
         'PASSWORD': get_env_variable('DB_PASSWORD'),
         'HOST': get_env_variable('DB_HOST'),
         'PORT': get_env_variable('DB_PORT'),
+        'CONN_MAX_AGE': 600,
     }
 }
 
