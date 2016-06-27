1261fb3d9c15578376fbe2a1a5f0a2f62e035c81
Eric Brelsford <ebrelsford@gmail.com>
2016-06-27 11:24:05 -0400

    Remove organizing status when lot has no organizers

--- nycommons/lots/signals.py
+++ nycommons/lots/signals.py
@@ -1,4 +1,4 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import pre_delete, post_save
 from django.dispatch import receiver
 
 from organize.models import Organizer
@@ -15,3 +15,19 @@ def update_organizing(sender, instance, **kwargs):
     lot = instance.content_object
     lot.organizing = True
     lot.save()
+
+
+@receiver(pre_delete, sender=Organizer,
+          dispatch_uid='lot_update_organizing_delete')
+def update_organizing_delete(sender, instance, **kwargs):
+    """
+    Once a Organizer is deleted, consider removing organizing status on lot.
+    """
+    if not instance:
+        return
+
+    lot = instance.content_object
+    
+    if not lot.organizers.exclude(pk=instance.pk).exists():
+        lot.organizing = False
+        lot.save()
