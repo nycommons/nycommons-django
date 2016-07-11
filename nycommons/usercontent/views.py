17e1216b8915c5138248c9ffea0564b36d509758
Eric Brelsford <ebrelsford@gmail.com>
2016-07-11 13:59:11 -0400

    Require login to use add-content views

--- nycommons/usercontent/views.py
+++ nycommons/usercontent/views.py
@@ -1,19 +1,26 @@
+from braces.views import LoginRequiredMixin, PermissionRequiredMixin
 from livinglots_usercontent.views import AddContentView
 
 from lots.models import Lot
 from .forms import FileForm, NoteForm, PhotoForm
 
 
-class AddFileView(AddContentView):
+class AddFileView(LoginRequiredMixin, PermissionRequiredMixin, AddContentView):
     content_type_model = Lot
     form_class = FileForm
+    login_url = '/admin/login/'
+    permission_required = 'files.add_file'
 
 
-class AddNoteView(AddContentView):
+class AddNoteView(LoginRequiredMixin, PermissionRequiredMixin, AddContentView):
     content_type_model = Lot
     form_class = NoteForm
+    login_url = '/admin/login/'
+    permission_required = 'notes.add_note'
 
 
-class AddPhotoView(AddContentView):
+class AddPhotoView(LoginRequiredMixin, PermissionRequiredMixin, AddContentView):
     content_type_model = Lot
     form_class = PhotoForm
+    login_url = '/admin/login/'
+    permission_required = 'photos.add_photo'
