b489644c4cb6265160ef759e00516e0649303f9f
Eric Brelsford <ebrelsford@gmail.com>
2016-07-11 13:21:32 -0400

    File and photo previews

--- nycommons/activities/views.py
+++ nycommons/activities/views.py
@@ -1,5 +1,7 @@
 from livinglots_activities.views import BaseActivityJSONListView
+from livinglots_usercontent.files.models import File
 from livinglots_usercontent.notes.models import Note
+from livinglots_usercontent.photos.models import Photo
 
 
 class ActivityJSONListView(BaseActivityJSONListView):
@@ -9,6 +11,10 @@ class ActivityJSONListView(BaseActivityJSONListView):
         d = super(ActivityJSONListView, self).get_action_dict(action)
         if action.target:
             d['target_url'] = action.target.get_absolute_url()
+        if action.action_object and type(action.action_object) is File:
+            d['file_url'] = action.action_object.document.url
         if action.action_object and type(action.action_object) is Note:
             d['details'] = action.action_object.text
+        if action.action_object and type(action.action_object) is Photo:
+            d['thumbnail_url'] = action.action_object.thumbnail.url
         return d
