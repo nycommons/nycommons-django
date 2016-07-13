ca8967db94014912358eae98ae1f7bdda7c32bba
Eric Brelsford <ebrelsford@gmail.com>
2016-07-13 11:48:02 -0400

    Require email for Organizer and Watcher instances

--- nycommons/organize/models.py
+++ nycommons/organize/models.py
@@ -4,6 +4,10 @@ from livinglots_organize.models import BaseOrganizer, BaseWatcher
 class Organizer(BaseOrganizer):
     pass
 
-
 class Watcher(BaseWatcher):
     pass
+
+
+# Require email fields for both Organizer and Watcher instances
+Organizer._meta.get_field('email').blank = False
+Watcher._meta.get_field('email').blank = False
