d5cb56bee98e537fb6dd3314de96aa26b667ad35
Eric Brelsford <ebrelsford@gmail.com>
2016-08-02 12:30:30 -0400

    More useful lots admin

--- nycommons/lots/admin.py
+++ nycommons/lots/admin.py
@@ -6,9 +6,33 @@ from .models import Lot
 
 
 class LotAdmin(BaseLotAdmin):
-    fieldsets = BaseLotAdmin.fieldsets + (
-        ('Ownership', {
-            'fields': ('owner',),
+    list_display = ('address_line1', 'city', 'name', 'commons_type',)
+    list_filter = ('commons_type', 'added', 'owner',)
+    readonly_fields = ('added', 'group', 'stewards_list',)
+    fieldsets = (
+        (None, {
+            'fields': (
+                ('bbl', 'name',),
+                ('address_line1', 'address_line2', 'city', 'borough',),
+                ('state_province', 'postal_code',),
+                ('added', 'added_reason',),
+                'group',
+            ),
+        }),
+        ('Commmons', {
+            'fields': ('commons_type', 'priority',),
+        }),
+        ('Known use', {
+            'classes': ('collapse',),
+            'fields': ('known_use', 'known_use_certainty', 'known_use_locked',),
+        }),
+        ('Stewards', {
+            'classes': ('collapse',),
+            'fields': ('stewards_list', 'steward_inclusion_opt_in',),
+        }),
+        ('Geography', {
+            'classes': ('collapse',),
+            'fields': ('centroid', 'polygon', 'polygon_area', 'polygon_width',),
         }),
     )
 
