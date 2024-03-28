aae54d18cb1f5c77c4a2343569f412dd0f061b99
Eric Brelsford <ebrelsford@gmail.com>
2024-03-27 23:04:14 -0400

    Update filter colors

--- nycommons/static/js/map/styles.js
+++ nycommons/static/js/map/styles.js
@@ -2,24 +2,53 @@
 // Lot map styles by layer for maps
 //
 
-var fillColors = {
-    library: '#057FC6',
-    park: '#4def3e',
-    'park building': '#997940',
-    'public housing': '#F5A623',
-    'post office': '#662D91',
-    'vacant lot / garden': '#1F9E48',
-    waterfront: '#E669EE',
-    default: '#000000'
-};
+var fillColors = [
+    {
+        keys: ['demolition_proposed', 'demolition_completed'],
+        color: '#000000',
+    },
+    {
+        keys: ['radpact_converted', 'radpact_planned'],
+        color: '#057FC6',
+    },
+    {
+        keys: ['preservation_trust_voting_planned', 'preservation_trust_complete'],
+        color: '#662D91',
+    },
+    {
+        keys: ['private_infill_planned', 'private_infill_completed'],
+        color: '#00C3FF',
+    },
+    {
+        keys: ['section_8_pre_2014'],
+        color: '#F66557',
+    },
+    {
+        keys: ['new_public_housing_built', 'new_public_housing_planned'],
+        color: '#4DEF3E',
+    },
+    {
+        keys: ['nycha_modernization_planned', 'nycha_modernization_complete'],
+        color: '#E669EE',
+    },
+];
+
+var defaultFill = '#F5A623';
 
 var priorityColor = '#bf1e2d';
 
-function getLayerColor (layer) {
-    if (fillColors[layer] !== undefined) {
-        return fillColors[layer];
+function getLayerColor(props) {
+    var matches = fillColors.filter(function (e) {
+        return e.keys.filter(function (k) {
+            return props[k];
+        }).length > 0;
+    });
+
+    if (matches.length > 0) {
+        return matches[0].color;
     }
-    return fillColors.default;
+
+    return defaultFill;
 }
 
 module.exports = {
@@ -33,11 +62,12 @@ module.exports = {
             fillOpacity: 1,
             stroke: false
         };
-        style.fillColor = getLayerColor(feature.properties.commons_type);
+        style.fillColor = getLayerColor(feature.properties);
 
         if (feature.properties.priority) {
             style.color = priorityColor;
             style.stroke = true;
+            style.weight = 1;
         }
         return style;
     }
