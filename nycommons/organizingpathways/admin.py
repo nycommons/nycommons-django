1da91ed72016d9b205864e9bf7272ff4963f9129
Eric Brelsford <ebrelsford@gmail.com>
2024-04-04 22:03:47 -0400

    Add private_infill_completed

--- nycommons/organizingpathways/admin.py
+++ nycommons/organizingpathways/admin.py
@@ -29,6 +29,7 @@ class OrganizingPathwayAdmin(BasePathwayAdmin):
                 'preservation_trust_voting_planned',
                 'preservation_trust_complete',
                 'private_infill_planned',
+                'private_infill_completed',
                 'section_8_pre_2014',
                 'demolition_proposed',
                 'demolition_completed',
